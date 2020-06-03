3ef37be39e9146d04ef8d9952984d16e2bb5947c
Victor <victorleonc@iqs.edu>
2020-06-03 17:16:17 +0200

    added ggmaps

--- pruebasGit.R
+++ pruebasGit.R
@@ -1,11 +1,19 @@
-library(tidyverse)
-library(leaflet)
+if(!requireNamespace("devtools")) install.packages("devtools")
+devtools::install_github("dkahle/ggmap")
 
+
+library(tidyverse)
+library(ggmap)
+
+
+#“terrain”, “terrain-background”, “terrain-labels”, “terrain-lines”, “toner”, 
+#“toner-2010”, “toner-2011”, “toner-background”, “toner-hybrid”, “toner-labels”, 
+#“toner-lines”, “toner-lite”, “watercolor”
 
 load("listings.Rdata")
 load("listings_detailed.Rdata")
 
-devtools::install_github("dkahle/ggmap", ref = "tidyup")
+
 
 height <- max(listings$latitude) - min(listings$latitude)
 width <- max(listings$longitude) - min(listings$longitude)
@@ -14,11 +22,9 @@ borders <- c(bottom  = min(listings$latitude)  - 0.1 * height,
              left    = min(listings$longitude) - 0.1 * width,
              right   = max(listings$longitude) + 0.1 * width)
 
-map <- get_stamenmap(borders, zoom = 11, maptype = "toner-lite")
+map <- get_stamenmap(borders, zoom = 11, maptype = "terrain")
 ggmap(map) +
   geom_point(data = listings, mapping = aes(x = longitude, y = latitude,
-                                          colour = room_type))
-
-
+                                          colour = room_type), alpha=0.5)
 
 
