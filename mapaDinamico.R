83c4653392f3d337bedb7a8785d6f1d855535b45
victorleonc <55855400+vleonc@users.noreply.github.com>
2020-06-03 19:10:21 +0200

    put required packages

--- mapaDinamico.R
+++ mapaDinamico.R
@@ -1,3 +1,7 @@
+list.of.packages <- c("leaflet", "tidyverse")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 library(tidyverse)
 library(leaflet)
 
