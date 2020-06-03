7ece56b1ead0a445c65e541a81c8c23b187d8570
Victor <victorleonc@iqs.edu>
2020-06-03 14:53:54 +0200

    added nyc map by room_type

--- pruebasGit.R
+++ pruebasGit.R
@@ -1,5 +1,24 @@
 library(tidyverse)
+library(leaflet)
+
 
 load("listings.Rdata")
 load("listings_detailed.Rdata")
+
+devtools::install_github("dkahle/ggmap", ref = "tidyup")
+
+height <- max(listings$latitude) - min(listings$latitude)
+width <- max(listings$longitude) - min(listings$longitude)
+borders <- c(bottom  = min(listings$latitude)  - 0.1 * height,
+             top     = max(listings$latitude)  + 0.1 * height,
+             left    = min(listings$longitude) - 0.1 * width,
+             right   = max(listings$longitude) + 0.1 * width)
+
+map <- get_stamenmap(borders, zoom = 11, maptype = "toner-lite")
+ggmap(map) +
+  geom_point(data = listings, mapping = aes(x = longitude, y = latitude,
+                                          colour = room_type))
+
+
+
 
