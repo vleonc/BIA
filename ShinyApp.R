5609d530ed74e8d08e14f1e8c6d313d44e19284b
Lorenzo Paso Baar-Baarenfels <lorenzo@Lorenzos-iMac.local>
2020-06-04 10:30:56 +0200

    Remove comments

--- ShinyApp.R
+++ ShinyApp.R
@@ -20,29 +20,20 @@ ui <- bootstrapPage(
 
 server <- function(input, output, session) {
   
-  # Reactive expression for the data subsetted to what the user selected
   filteredData <- reactive({
     listings[listings$price >= input$range[1] & listings$price <= input$range[2],]
   })
   
-  # This reactive expression represents the palette function,
-  # which changes as the user makes selections in UI.
   colorpal <- reactive({
     colorNumeric(input$colors, listings$price)
   })
   
   output$map <- renderLeaflet({
-    # Use leaflet() here, and only include aspects of the map that
-    # won't need to change dynamically (at least, not unless the
-    # entire map is being torn down and recreated).
     leaflet(listings) %>% addTiles() %>%
       fitBounds(~min(longitude), ~min(latitude), ~max(longitude), ~max(latitude))
   })
   
-  # Incremental changes to the map (in this case, replacing the
-  # circles when a new color is chosen) should be performed in
-  # an observer. Each independent set of things that can change
-  # should be managed in its own observer.
+
   observe({
     pal <- colorpal()
     
@@ -75,12 +66,9 @@ server <- function(input, output, session) {
       )
   })
   
-  # Use a separate observer to recreate the legend as needed.
   observe({
     proxy <- leafletProxy("map", data = listings)
     
-    # Remove any existing legend, and only if the legend is
-    # enabled, create a new one.
     proxy %>% clearControls()
     if (input$legend) {
       pal <- colorpal()
